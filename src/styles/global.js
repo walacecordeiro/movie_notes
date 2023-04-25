0774fa198780810809a3a093ebd61fe99dec1f13
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-04-25 10:01:39 -0300

    I determined the font schema

--- src/styles/global.js
+++ src/styles/global.js
@@ -7,22 +7,38 @@ export default createGlobalStyle`
         box-sizing: border-box;
     }
 
+    :root {
+        font-size: 62.5%;
+    }
+
     body {
-        background-color: ${({theme}) => theme.COLORS.BACKGROUND_PRIMARY};
+        background-color: ${({ theme }) => theme.COLORS.BACKGROUND_PRIMARY};
 
-        color: ${({theme}) => theme.COLORS.TEXT};
+        color: ${({ theme }) => theme.COLORS.TEXT};
 
-        a {
-            text-decoration: none;
-        }
+        -webkit-font-smoothing: antialiased;
+    }
 
-        button, a {
-            cursor: pointer;
-            transition: filtter 0.2s;
-        }
+    body, input, button, textarea {
+        font-family: 'Roboto Slab', serif;
+        font-size: 1.6rem;
+        outline: none;
+    }
+    
+    p {
+        font-family: 'Roboto', sans-serif;
+    }
 
-        button:hover, a:hover {
-            filter: brightness(0.9);
-        }
+    a {
+        text-decoration: none;
+    }
+
+    button, a {
+        cursor: pointer;
+        transition: filtter 0.2s;
+    }
+
+    button:hover, a:hover {
+        filter: brightness(0.9);
     }
 `

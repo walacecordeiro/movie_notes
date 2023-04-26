d47453126a861b9701feed30286e30f8db210417
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-04-26 18:31:37 -0300

    finished the 'Details' page

--- src/pages/details/styles.js
+++ src/pages/details/styles.js
@@ -5,24 +5,41 @@ export const Container = styled.div`
     height: 100vh;
 
     display: grid;
-    grid-template-rows: 10.5rem;
+    grid-template-rows: 11.6rem;
     grid-template-areas: 
     "header"
     "content"
     ;
 
-    .title {
-        display: flex;
-        align-items: center;
-        gap: 1.9rem;
-        margin-top: 2.4rem;
+    > main {
+        grid-area: content;
+        overflow-y: auto;
+        padding: 4rem 0;
     }
+`
+
+export const Content = styled.div`
+    max-width: 111.3rem;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+`
+
+export const TitleAndRating=styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1.9rem;
+    margin-top: 2.4rem;
 `
 
 export const Author = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
+
+    padding-top: 2.4rem;
 
     gap: .8rem;
 
@@ -42,4 +59,7 @@ export const Author = styled.div`
         width: 1.6rem;
         color: ${({ theme }) => theme.COLORS.PINK_GLOBAL};
     }
+`
+export const Tags=styled.div`
+    padding: 4rem 0;
 `

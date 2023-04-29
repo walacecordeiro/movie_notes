b89fb1e4f68a12acaaca1c23a1cda50b0f04b252
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-04-29 09:44:39 -0300

    application routes created; authentication routes created

---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,18 @@
+import {Routes, Route} from "react-router-dom"
+
+import { Details } from "../pages/details"
+import { Home } from "../pages/home"
+import { NewMovie } from "../pages/newMovie"
+import { Profile } from "../pages/profile"
+
+export function AppRoutes(){
+    return (
+        <Routes>
+            <Route element={<Details />} path="/detalhes" />
+            <Route element={<Home />} path="/" />
+            <Route element={<NewMovie />} path="/adicionar-filme" />
+            <Route element={<Profile />} path="/perfil" />
+           
+        </Routes>
+    )
+}
---
+++ src/routes/auth.routes.jsx
@@ -0,0 +1,13 @@
+import { Router, Route, Routes} from "react-router-dom"
+
+import { SignIn } from "../pages/signIn"
+import { SignUp } from "../pages/signUp"
+
+export function AuthRoutes(){
+    return (
+        <Routes>
+            <Route element={<SignIn />} path="/"/>
+            <Route element={<SignUp />} path="/registrar"/>
+        </Routes>
+    )
+}

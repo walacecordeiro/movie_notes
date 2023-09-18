0e92549a328ee42a97e3f7cacb4f1f8dfe025994
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-09-18 19:23:25 -0300

    apenas adicionei uma animação para deixar bonito

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,4 +1,15 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`
 
 export const Container = styled.div`
     width: 100%;
@@ -38,6 +49,8 @@ export const Content = styled.div`
 
   overflow: auto;
 
+  animation: ${fadeIn} 1s ease-in-out;
+  
   h1 span {
     font-size: 2.4rem;
   }
